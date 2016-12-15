d32051487b90468469d759102cda480aac1ff939
hiroshi yamazaki <hiroshi.yamazaki@jp.oro.com>
2016-12-15 11:14:20 +0900

    fix snapshot script

--- aws-mt/roles/backup/templates/bin/create_snapshot.py
+++ aws-mt/roles/backup/templates/bin/create_snapshot.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 #encoding: utf-8
 
+# This task requires follow IAM Policies
+# 
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ec2:Describe*",
+#                 "ec2:CreateSnapshot",
+#                 "ec2:DeleteSnapshot",
+#                 "ec2:CreateTags",
+#                 "ec2:DeleteTags"
+#             ],
+#             "Resource": "*"
+#         }
+#     ]
+# }
+
 import boto.ec2
 
 region = '{{ ec2.region }}'
